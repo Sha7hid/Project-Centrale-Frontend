59740a606b6906d6d04c40427b57e0ab12b0261c
sha7hid <shahidshafadshahad@gmail.com>
2024-01-17 21:41:43 +0530

    approval feature added

--- centrale/ProjectStatus.js
+++ centrale/ProjectStatus.js
@@ -18,6 +18,11 @@ export default function ProjectStatus({ navigation }) {
   const [studentData,setStudentData] = useState(null)
   const [teamData,setTeamData] = useState(null)
   const [projectData,setProjectData] = useState(null)
+  const[success, setSuccess] = useState(null)
+  const[success2, setSuccess2] = useState(null)
+  const[success3, setSuccess3] = useState(null)
+  const[success4, setSuccess4] = useState(null)
+  const[success5, setSuccess5] = useState(null)
   // const [pdfPath, setPdfPath] = React.useState(null);
   // const downloadPDF = async () => {
   //   const fileURL = 'YOUR_GOOGLE_DRIVE_PUBLIC_PDF_URL'; // Replace with the public file URL
@@ -52,8 +57,116 @@ export default function ProjectStatus({ navigation }) {
   //     console.error('Error extracting text from PDF:', error);
   //   }
   // };
-
-
+  const handleSubmit = () => {
+   
+    const apiUrl = `https://centrale.onrender.com/project/synopsis/approval/update/teamid/${teamData.id}`;
+  
+    const requestData = {
+      synopsisApproval:1
+    };
+  
+    fetch(apiUrl, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then(response => response.json())
+      .then(setSuccess(true))
+      .catch(error => {
+        // Handle any errors that occur during the fetch
+        console.error('Error:', error);
+      });
+  };
+  const handleSubmit2 = () => {
+   
+    const apiUrl = `https://centrale.onrender.com/project/design/approval/update/teamid/${teamData.id}`;
+  
+    const requestData = {
+      designApproval:1
+    };
+  
+    fetch(apiUrl, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then(response => response.json())
+      .then(setSuccess2(true))
+      .catch(error => {
+        // Handle any errors that occur during the fetch
+        console.error('Error:', error);
+      });
+  };
+  const handleSubmit3 = () => {
+   
+    const apiUrl = `https://centrale.onrender.com/project/codephase1/approval/update/teamid/${teamData.id}`;
+  
+    const requestData = {
+      codephase1Approval:1
+    };
+  
+    fetch(apiUrl, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then(response => response.json())
+      .then(setSuccess3(true))
+      .catch(error => {
+        // Handle any errors that occur during the fetch
+        console.error('Error:', error);
+      });
+  };
+  const handleSubmit4 = () => {
+   
+    const apiUrl = `https://centrale.onrender.com/project/codephase2/approval/update/teamid/${teamData.id}`;
+  
+    const requestData = {
+      codephase2Approval:1
+    };
+  
+    fetch(apiUrl, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then(response => response.json())
+      .then(setSuccess4(true))
+      .catch(error => {
+        // Handle any errors that occur during the fetch
+        console.error('Error:', error);
+      });
+  };
+  const handleSubmit5 = () => {
+   
+    const apiUrl = `https://centrale.onrender.com/project/report/approval/update/teamid/${teamData.id}`;
+  
+    const requestData = {
+      reportApproval:1
+    };
+  
+    fetch(apiUrl, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then(response => response.json())
+      .then(setSuccess5(true))
+      .catch(error => {
+        // Handle any errors that occur during the fetch
+        console.error('Error:', error);
+      });
+  };
   const fetchProjectData = (teamId) => {
     const apiUrl = `https://centrale.onrender.com/project/teamid/${teamId}`;
     fetch(apiUrl)
@@ -104,17 +217,17 @@ console.log(projectData)
 const getCompletionPercentage = () => {
     if (projectData?.synopsis && projectData?.design && projectData?.first_presentation && projectData?.codephase1 && projectData?.second_presentation && projectData?.codephase2 && projectData?.final_presentation && projectData?.report) {
       return <Text style={styles.cardtext}>100%</Text>; 
-    } else if (projectData?.synopsis && projectData?.design && projectData?.first_presentation && projectData?.codephase1 && projectData?.second_presentation && projectData?.codephase2 && projectData?.final_presentation) {
+    } else if (projectData?.synopsisApproval && projectData?.designApproval && projectData?.first_presentation && projectData?.codephase1Approval && projectData?.second_presentation && projectData?.codephase2Approval && projectData?.final_presentation) {
       return <Text style={styles.cardtext}>88%</Text>;
-    } else if (projectData?.synopsis && projectData?.design && projectData?.first_presentation && projectData?.codephase1 && projectData?.second_presentation && projectData?.codephase2) {
+    } else if (projectData?.synopsisApproval && projectData?.designApproval && projectData?.first_presentation && projectData?.codephase1Approval && projectData?.second_presentation && projectData?.codephase2Approval) {
       return <Text style={styles.cardtext}>76%</Text>;
-    }   else if (projectData?.synopsis && projectData?.design && projectData?.first_presentation && projectData?.codephase1 && projectData?.second_presentation ) {
+    }   else if (projectData?.synopsisApproval && projectData?.designApproval && projectData?.first_presentation && projectData?.codephase1Approval && projectData?.second_presentation ) {
       return <Text style={styles.cardtext}>60%</Text>;
-    }   else if (projectData?.synopsis && projectData?.design && projectData?.first_presentation && projectData?.codephase1) {
+    }   else if (projectData?.synopsisApproval && projectData?.designApproval && projectData?.first_presentation && projectData?.codephase1Approval) {
       return <Text style={styles.cardtext}>48%</Text>;
-    }   else if (projectData?.synopsis && projectData?.design && projectData?.first_presentation ) {
+    }   else if (projectData?.synopsisApproval && projectData?.designApproval && projectData?.first_presentation ) {
       return <Text style={styles.cardtext}>36%</Text>;
-    }   else if (projectData?.synopsis && projectData?.design  ) {
+    }   else if (projectData?.synopsisApproval && projectData?.designApproval) {
       return <Text style={styles.cardtext}>24%</Text>;
     }  else if (projectData?.synopsisApproval) {
       return <Text style={styles.cardtext}>12%</Text>;
@@ -151,13 +264,15 @@ console.log(studentData)
             </View>
 :<View></View>}
 <View style={styles.space}></View>
-{studentData?.type=='teacher'? <View style={styles.buttonwhite}>
-              <Pressable>
-                <Text style={styles.textblue}>Approve</Text>
+{studentData?.type=='teacher' && projectData?.synopsis ? <View style={styles.buttonwhite}>
+              <Pressable onPress={handleSubmit}>
+                <Text style={styles.textblue}>{projectData?.synopsisApproval? "Approved✅" :"Approve"}</Text>
               </Pressable>
             </View>:<View></View>}
             </View>
+            {success?<Text style={styles.text}>Approved Successfully</Text>:<Text></Text>}
    <View style={styles.spacetop}></View>
+   <View style={styles.cardlayout}>
      {projectData?.design ?
             <View style={styles.button2}>
               <Pressable onPress={()=> {
@@ -167,7 +282,16 @@ console.log(studentData)
               </Pressable>
             </View>
 :<View></View>}
+<View style={styles.space}></View>
+{studentData?.type=='teacher' && projectData?.design ? <View style={styles.buttonwhite}>
+              <Pressable onPress={handleSubmit2}>
+                <Text style={styles.textblue}>{projectData?.designApproval? "Approved✅" :"Approve"}</Text>
+              </Pressable>
+            </View>:<View></View>}
+</View>
+{success2?<Text style={styles.text}>Approved Successfully</Text>:<Text></Text>}
    <View style={styles.spacetop}></View>
+   <View style={styles.cardlayout}>
      {projectData?.codephase1 ?
             <View style={styles.button2}>
               <Pressable onPress={()=> {
@@ -177,7 +301,16 @@ console.log(studentData)
               </Pressable>
             </View>
 :<View></View>}
+<View style={styles.space}></View>
+{studentData?.type=='teacher' && projectData?.codephase1 ? <View style={styles.buttonwhite}>
+              <Pressable onPress={handleSubmit3}>
+                <Text style={styles.textblue}>{projectData?.codephase1Approval? "Approved✅" :"Approve"}</Text>
+              </Pressable>
+            </View>:<View></View>}
+</View>
+{success3?<Text style={styles.text}>Approved Successfully</Text>:<Text></Text>}
    <View style={styles.spacetop}></View>
+   <View style={styles.cardlayout}>
 {projectData?.codephase2 ?
             <View style={styles.button2}>
               <Pressable onPress={()=> {
@@ -187,7 +320,16 @@ console.log(studentData)
               </Pressable>
             </View>
 :<View></View>}
+<View style={styles.space}></View>
+{studentData?.type=='teacher' && projectData?.codephase2 ? <View style={styles.buttonwhite}>
+              <Pressable onPress={handleSubmit4}>
+                <Text style={styles.textblue}>{projectData?.codephase2Approval? "Approved✅" :"Approve"}</Text>
+              </Pressable>
+            </View>:<View></View>}
+</View>
+{success4?<Text style={styles.text}>Approved Successfully</Text>:<Text></Text>}
    <View style={styles.spacetop}></View>
+   <View style={styles.cardlayout}>
    {projectData?.report ?
             <View style={styles.button2}>
               <Pressable onPress={()=> {
@@ -197,6 +339,14 @@ console.log(studentData)
               </Pressable>
             </View>
 :<View></View>}
+<View style={styles.space}></View>
+{studentData?.type=='teacher' && projectData?.report ? <View style={styles.buttonwhite}>
+              <Pressable onPress={handleSubmit5}>
+                <Text style={styles.textblue}>{projectData?.reportApproval? "Approved✅" :"Approve"}</Text>
+              </Pressable>
+            </View>:<View></View>}
+</View>
+{success5?<Text style={styles.text}>Approved Successfully</Text>:<Text></Text>}
         </View>
       </ScrollView>
     </SafeAreaView>
